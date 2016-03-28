0a2d3569c00fa2e74a1db48f90633fbbd43ccc50
jbranchaud <jbranchaud@gmail.com>
2016-03-28 11:52:30 -0500

    Add example of using citext for case-insensitive emails

---
+++ citext-emails.sql
@@ -0,0 +1,40 @@
+-- user emails with citext
+
+begin;
+
+create extension if not exists citext;
+
+create table citext_emails (
+  id serial primary key,
+  email citext not null unique
+);
+-- \d citext_emails
+--                          Table "public.citext_emails"
+--  Column |  Type   |                         Modifiers
+-- --------+---------+------------------------------------------------------------
+--  id     | integer | not null default nextval('citext_emails_id_seq'::regclass)
+--  email  | citext  | not null
+-- Indexes:
+--     "citext_emails_pkey" PRIMARY KEY, btree (id)
+--     "citext_emails_email_key" UNIQUE CONSTRAINT, btree (email)
+
+insert into citext_emails (email)
+values
+  ('LizLemon@nbc.com'),
+  ('kennethparcell@nbc.com'),
+  ('JACK_DONAGHY@NBC.COM')
+;
+
+select * from citext_emails where email = 'lizlemon@nbc.com';
+--  id |      email
+-- ----+------------------
+--   1 | LizLemon@nbc.com
+
+select * from citext_emails where email ilike '%donaghy%';
+--  id |        email
+-- ----+----------------------
+--   3 | JACK_DONAGHY@NBC.COM
+
+--insert into citext_emails (email) values ('KENNETHparcell@nbc.COM');
+-- ERROR:  duplicate key value violates unique constraint "citext_emails_email_key"
+-- DETAIL:  Key (email)=(KENNETHparcell@nbc.COM) already exists.
